81b40e383a94276ab8c2e4c06f3977969a15ee47
aoguai <1340219674@qq.com>
2023-08-21 18:57:35 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,85 @@
+# rime_kaomoji_dict
+
+一个颜文字 (kaomoji) 词库，同时可以生成适用于 [Rime 输入法](https://rime.im/) 的词库。
+
+A collection of kaomoji expressions, also capable of generating dictionaries suitable for the [Rime input method](https://rime.im/).
+
+## 目录结构
+
+以下是对主要目录结构的说明:
+
+```html
+rime_kaomoji_dict
+├─ data
+│    ├─ A_kaomoji_dict_data.txt：A岛匿名版部分颜文字素材
+│    ├─ Temreg_dict_data.txt：百度贴吧吧友Temreg分享的颜文字素材
+│    ├─ custom_phrase_dict_data.txt： girhub kaos 项目中的颜文字素材
+│    ├─ lmeee_dict_data.txt：拉米工具颜文字素材
+│    └─ sougou_dict_data.txt：搜狗颜文字素材
+└─ main.py：处理颜文字素材的脚本
+```
+## 安装与使用
+本项目不仅可以用于生成适用于 [Rime 输入法](https://rime.im/) 的词库，也可以用于生成其他输入法的词库。
+
+同时您还可以将其视为一般颜文字词库，用于网络表情 NLP ，颜文字识别，颜文字表情实体识别、属性检测、新颜发现等。
+
+### 生成适用于 Rime 输入法的词库
+
+一般情况下如果您没有开发需求，您可以直接前往  [releases](https://github.com/aoguai/rime_kaomoji_dict/releases) 下载最新版本的词库文件，直接使用即可。
+
+解压后你会得到两个适用于 Rime 输入法的词库文件
+```html
+kaomoji_kmj.dict.yaml
+kaomoji_pinyin.dict.yaml
+```
+其中 `kaomoji_kmj.dict.yaml` 是颜文字词库，`kaomoji_pinyin.dict.yaml` 是颜文字拼音词库。
+
+它们的区别在于：
+
+`kaomoji_kmj.dict.yaml`：固定将**所有颜文字** **拼音部份（即文字对应的编码）** 设置为`kmj`
+
+`kaomoji_pinyin.dict.yaml` ：则是正常使用 [Pypinyin](https://github.com/mozillazg/python-pinyin) 生成所有颜文字对应的中文的拼音作为文字对应的编码
+
+二者互不冲突，您可以根据自己的喜好选择其一或全部将其放入 Rime 输入法的用户文件夹中并做对应的**调用**即可。
+
+### 从项目仓库源码构建
+
+同时您还可以自行从项目仓库源码构建词库
+
+```bash
+git clone https://github.com/aoguai/rime_kaomoji_dict.git
+cd rime_kaomoji_dict
+pip install -r requirements.txt
+python main.py
+```
+完成后，**默认**会在data 目录中生成
+```html
+all_output_result_kmj.txt
+all_output_result_pinyin.txt
+```
+与 `kaomoji_kmj.dict.yaml`和 `kaomoji_pinyin.dict.yaml`对应
+
+同时，从本仓库源码构建时，您可以自行选择使用哪些颜文字素材，只需在 `main.py` 中修改 `input_filename_list` 变量即可。
+
+同时你还可以自定义导出的词库的**格式**等，具体请自行查看 `main.py` 中的 `process_and_save_combined_kaomoji` 函数部分代码。
+
+## 数据原始来源
+
+[X岛匿名版](https://www.nmbxd1.com/Forum)
+
+[颜文字大全 - 拉米工具](https://tool.lmeee.com/yanwenzi)
+
+[求助小狼毫怎么输入颜文字。【rime吧】_百度贴吧](https://tieba.baidu.com/p/2357282768)
+
+[kaos/dict.txt at master · tisyang/kaos](https://github.com/tisyang/kaos/blob/master/dict.txt)
+
+[搜狗颜文字](https://pinyin.sogou.com/dict/ywz/?f=dict_index&ytype=24)
+
+## 免责声明
+此存储库遵循 [MIT 开源协议](https://github.com/aoguai/rime_kaomoji_dict/blob/main/LICENSE)，请务必理解。
+
+我们严禁所有通过本程序违反任何国家法律的行为，请在法律范围内使用本程序。
+
+默认情况下，使用此项目将被视为您同意我们的规则。请务必遵守道德和法律标准。
+
+如果您不遵守，您将对后果负责，作者将不承担任何责任！
---
+++ main.py
@@ -0,0 +1,154 @@
+import re
+from pypinyin import pinyin, Style
+
+
+def process_and_save_combined_kaomoji(input_filename, output_filename, is_pinyin=True, save_file=True):
+    """
+    处理并保存合并后的颜文字表情
+
+    :param input_filename: 输入文件名
+    :param output_filename: 输出文件名
+    :param is_pinyin: 是否以拼音保存
+    :param save_file: 是否保存到文件
+
+    :return: 保存到文件的结果列表
+    """
+
+    # rime词库规定某些前缀无法作为开头需要删除
+    def remove_prefix(text, prefix):
+        while text.startswith(prefix):
+            text = text[len(prefix):]
+        return text
+
+    # 读取文本文件
+    with open(input_filename, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    pattern = None
+    chinese_english_pattern = None
+    # 定义正则表达式模式
+    if 'lmeee' in input_filename:
+        pattern = r'<p>(.*?)<\/p><span class="copyBtn".*?data-desc="(.*?)".*?data-clipboard-text=".*?">.*?<\/span>'
+        chinese_english_pattern = r'^[a-zA-Z\u4e00-\u9fa5]+$'
+    elif 'Temreg' in input_filename:
+        pattern = r'^(.*?)    (.*)$'
+    elif 'A_kaomoji' in input_filename:
+        pattern = None
+    elif 'custom_phrase' in input_filename:
+        pattern = r'^(.*?)\t(.*)\t(.*)$'
+    elif 'sougou' in input_filename:
+        pattern = r'<div class="ywz_content">(.*?)<\/div>.*?<div class="ywz_cont_name">输入文字：(.*?)<\/div>'
+        chinese_english_pattern = r'^[a-zA-Z\u4e00-\u9fa5]+$'
+
+    output_result_pinyin = []
+    output_result_kmj = []
+
+    # 遍历每一行并进行匹配
+    for line in lines:
+        if pattern:
+            match = re.search(pattern, line)
+            if match:
+                if 'lmeee' in input_filename or 'sougou' in input_filename:
+                    emoticon = remove_prefix(remove_prefix(match.group(1).strip(), "---"), "...")  # 提取颜文字表情,同时排除非法开头
+                    chinese_text = match.group(2)  # 提取对应的中文
+                    if is_pinyin:
+                        # 去除空格后，检查中文或英文字符是否占据整个字符串
+                        if re.match(chinese_english_pattern, chinese_text.replace(" ", "")):
+                            # 使用PyPinyin库将中文转换为拼音
+                            pinyin_text = pinyin(chinese_text, style=Style.NORMAL, heteronym=False)
+                            pinyin_str = ''.join([item[0] for item in pinyin_text])
+
+                            # 格式化并写入到结果列表
+                            output_line = f"{emoticon}\t{pinyin_str}\t1\n"
+                            output_result_pinyin.append(output_line)
+                        else:
+                            continue
+                    else:
+                        output_line = f"{emoticon}\tkmj\t1\n"
+                        output_result_kmj.append(output_line)
+                elif 'Temreg' in input_filename:
+                    chinese_text = match.group(1)  # 提取拼音
+                    emoticon = remove_prefix(remove_prefix(match.group(2).strip(), "---"), "...")  # 提取颜文字表情,同时排除非法开头
+                    if is_pinyin:
+                        # 格式化并写入到结果列表
+                        output_line = f"{emoticon}\t{chinese_text}\t1\n"
+                        output_result_pinyin.append(output_line)
+                    else:
+                        output_line = f"{emoticon}\tkmj\t1\n"
+                        output_result_kmj.append(output_line)
+                elif 'custom_phrase' in input_filename:
+                    emoticon = remove_prefix(remove_prefix(match.group(1).strip(), "---"), "...")  # 提取颜文字表情,同时排除非法开头
+                    chinese_text = match.group(2)  # 提取拼音
+                    if is_pinyin:
+                        # 格式化并写入到结果列表
+                        output_line = f"{emoticon}\t{chinese_text}\t1\n"
+                        output_result_pinyin.append(output_line)
+                    else:
+                        output_line = f"{emoticon}\tkmj\t1\n"
+                        output_result_kmj.append(output_line)
+        elif 'A_kaomoji' in input_filename:
+            emoticon = remove_prefix(remove_prefix(line.strip(), "---"), "...")  # 提取颜文字表情,同时排除非法开头
+            output_line = f"{emoticon}\tkmj\t1\n"
+            output_result_kmj.append(output_line)
+
+    # 保存结果到文件
+    if save_file:
+        with open(output_filename, 'w', encoding='utf-8') as output_file:
+            if is_pinyin:
+                if 'A_kaomoji' in input_filename:
+                    raise ValueError("A_kaomoji does not support pinyin conversion.")
+                if output_result_pinyin:
+                    output_file.writelines(output_result_pinyin)
+            else:
+                if output_result_kmj:
+                    output_file.writelines(output_result_kmj)
+        print(f"结果已保存到{output_filename}文件中")
+
+    return output_result_pinyin, output_result_kmj
+
+
+if __name__ == "__main__":
+
+    def get_pinyin_key(item):
+        parts = item.split("\t")
+        chinese_pinyin = parts[1]
+        return chinese_pinyin.lower()  # 转换为小写进行来忽略大小写
+
+
+    all_output_result_pinyin = []
+    all_output_result_kmj = []
+
+    input_filename_list = ['data/A_kaomoji_dict_data.txt', 'data/custom_phrase_dict_data.txt',
+                           'data/lmeee_dict_data.txt', 'data/sougou_dict_data.txt', 'data/Temreg_dict_data.txt']
+    for input_filename in input_filename_list:
+        output_result_pinyin, output_result_kmj = process_and_save_combined_kaomoji(input_filename=input_filename,
+                                                                                    output_filename=input_filename.replace(
+                                                                                        '.txt', '_output_file.txt'),
+                                                                                    is_pinyin=False, save_file=False)
+        all_output_result_pinyin.extend(output_result_pinyin)
+        all_output_result_kmj.extend(output_result_kmj)
+        if not 'A_kaomoji' in input_filename:
+            output_result_pinyin, output_result_kmj = process_and_save_combined_kaomoji(input_filename=input_filename,
+                                                                                        output_filename=input_filename.replace(
+                                                                                            '.txt', '_output_file.txt'),
+                                                                                        is_pinyin=True, save_file=False)
+            all_output_result_pinyin.extend(output_result_pinyin)
+            all_output_result_kmj.extend(output_result_kmj)
+
+    # 使用集合进行去重
+    all_output_result_pinyin = list(set(all_output_result_pinyin))
+    all_output_result_kmj = list(set(all_output_result_kmj))
+
+    # 排序
+    all_output_result_pinyin = sorted(all_output_result_pinyin, key=get_pinyin_key)
+    all_output_result_kmj = sorted(all_output_result_kmj, key=get_pinyin_key)
+
+    print(len(all_output_result_pinyin), len(all_output_result_kmj))
+
+    with open('data/all_output_result_pinyin.txt', 'w', encoding='utf-8') as output_file:
+        output_file.writelines(all_output_result_pinyin)
+        print(f"拼音字典结果已保存到data/all_output_result_pinyin.txt文件中")
+
+    with open('data/all_output_result_kmj.txt', 'w', encoding='utf-8') as output_file:
+        output_file.writelines(all_output_result_kmj)
+        print(f"kmj字典结果已保存到data/all_output_result_kmj.txt文件中")

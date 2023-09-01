d9199be10e5a1bc7230dc42e45ecca50b7f95e22
xtmu <90405292+xtmu@users.noreply.github.com>
2023-09-02 06:15:23 +0800

    添加功能：全拼字典转换成双拼字典
    支持小鹤、自然码、搜狗、微软、智能ABC 双拼方案。由于音节划分算法有局限，部分形如 pingan 的编码会取 pin/gan 而非 ping/an 导致转换不正确。

--- pyshuangpin/__init__.py
+++ pyshuangpin/__init__.py
@@ -24,3 +24,36 @@ def shuangpin(hans, scheme: Scheme, **kwargs):
                 item[index] = item[index].replace(key, value)
 
     return pinyin
+def shuangpin_by_syllabl(syllablelist, scheme: Scheme, **kwargs):
+
+    """
+    使用全拼音节列表导出双拼编码
+    """
+    def l2ll(l):
+        ll = []
+        for index in range(len(l)):
+            ll.append([l[index]])
+        # print(ll)
+        return ll
+
+    pinyin=l2ll(syllablelist)
+
+    if scheme == Scheme.Xiaohe:
+        scheme_dict = xiaohe.scheme
+    elif scheme == Scheme.Ziranma:
+        scheme_dict = ziranma.scheme
+    elif scheme == Scheme.Sogou:
+        scheme_dict = sogou.scheme
+    elif scheme == Scheme.Microsoft:
+        scheme_dict = microsoft.scheme
+    elif scheme == Scheme.ZNABC:
+        scheme_dict = znabc.scheme
+    else:
+        raise NotImplementedError('scheme not implemented')
+
+    for key, value in scheme_dict.items():
+        for item in pinyin:
+            for index in range(len(item)):
+                item[index] = item[index].replace(key, value)
+
+    return pinyin
